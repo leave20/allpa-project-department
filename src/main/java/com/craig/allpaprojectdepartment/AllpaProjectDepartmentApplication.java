7b89a305dc18e6dfedeef370c4016d66c8f82381
ccastrop <ccastrop@everis.com>
2022-09-10 14:02:37 -0500

    feat: update files

--- src/main/java/com/craig/allpaprojectdepartment/AllpaProjectDepartmentApplication.java
+++ src/main/java/com/craig/allpaprojectdepartment/AllpaProjectDepartmentApplication.java
@@ -2,8 +2,10 @@ package com.craig.allpaprojectdepartment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AllpaProjectDepartmentApplication {
 
     public static void main(String[] args) {
